33d5cdade40c93d8cd8f5a6906ed73fb50d9258e
Sandeep Suresh <sureshsa@usc.edu>
2018-04-12 19:15:19 -0700

    Starting to create the server.

---
+++ src/MainServer.java
@@ -0,0 +1,23 @@
+
+public class MainServer {
+
+	public static void main(String[] args) {
+		//Connect to Database
+		
+		//Get List of APIs and Access Tokens
+		
+		//Make threads for each API
+		
+		//Execute each thread to make call to said API
+		
+
+		
+		
+	}
+	
+	public void updateAPI(String api_name) {
+		
+		//Switch statement
+	}
+
+}
