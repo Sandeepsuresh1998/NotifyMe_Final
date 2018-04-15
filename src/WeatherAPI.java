f27e3a733679e4bbfcde2fa5f3737d72d8eb9cfd
Sandeep Suresh <sureshsa@usc.edu>
2018-04-14 18:16:27 -0700

    I am adding my WeatherAPI class.

---
+++ src/WeatherAPI.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.invoke.VarHandle;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.websocket.Session;
+import com.google.api.client.json.Json;
+import com.google.api.services.youtube.model.VideoContentDetails;
+import com.google.gson.Gson;
+
+public class WeatherAPI extends Thread{
+	private Session mSession;
+	
+	public WeatherAPI(Session s) {
+		this.mSession = s;
+		this.start();
+	}
+	
+	public void run() {
+		String weatherURL = "http://api.openweathermap.org/data/2.5/weather?id=5368381&APPID=afbcfd3e650101530d7d00ed5956e2e0";
+		try {
+			URL url = new URL(weatherURL);
+	        URLConnection yc = url.openConnection();
+	        BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+	        String inputLine;
+	        String response = "";
+	        while ((inputLine = in.readLine()) != null) {
+	        		response += inputLine;
+	        }
+	        System.out.println(response);
+	        String weatherData = "Weather|" + response;
+	        mSession.getBasicRemote().sendText(weatherData);
+	            
+		} catch (IOException ioe) {
+			// TODO Auto-generated catch block
+			ioe.printStackTrace();
+		}
+	}
+	
+	public double kelvinToFahrenheit(double temp) {
+		return (temp * (9.0/5.0) - 459.67);
+	}
+}
